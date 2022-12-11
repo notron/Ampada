5ece2da50862b069971c6449c729bd2c53e0241d
notron021@gmail.com <notron021@gmail.com>
2022-12-11 12:52:53 +0330

    create login page UI

---
+++ Ampada/View/Views/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Ampada
+//
+//  Created by Mahdi on 12/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
